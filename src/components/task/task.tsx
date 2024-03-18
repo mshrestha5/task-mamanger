3e9c5f9055dd9ed949f4fb4c4c3316be05bc6a52
Matina <shresthamatina21@gmail.com>
2024-03-18 16:01:45 -0500

    adding proprs to task componenet

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,19 +1,33 @@
 import React, { FC, ReactElement } from 'react';
-import { TaskHeader } from './_taskHeader';
-import { TaskDescription } from './_taskDescription';
 
 import { Box } from '@mui/material';
+import { ITask } from './interfaces/ITask';
+import { Priority } from '../createTaskForm/enums/Priority';
+import { Status } from '../createTaskForm/enums/Status';
+import { TaskDescription } from './_taskDescription';
 import { TaskFooter } from './_taskFooter';
+import { TaskHeader } from './_taskHeader';
 
-export const Task: FC = (): ReactElement => {
+export const Task: FC<ITask> = (props): ReactElement => {
+  //  Destructure props
+  const {
+    title = 'Test Title',
+    date = new Date(),
+    description = 'Lorem ipsum dolor sit amet',
+    priority = Priority.normal,
+    status = Status.completed,
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
     <Box
       display="flex"
       width="100%"
       justifyContent="flex-start"
       flexDirection="column"
-      mb={2}
-      p={4}
+      mb={4}
+      p={2}
       sx={{
         width: '100%',
         backgroundColor: 'background.paper',
@@ -22,10 +36,12 @@ export const Task: FC = (): ReactElement => {
         borderColor: 'error.light',
       }}
     >
-      Test
-      <TaskHeader/>
-      <TaskDescription/>
-      <TaskFooter/>
+      <TaskHeader title={title} date={date} />
+      <TaskDescription description={description} />
+      <TaskFooter
+        onClick={onClick}
+        onStatusChange={onStatusChange}
+      />
     </Box>
   );
 };
